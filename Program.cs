9dc6dbd8c7179fe5623e79394a0377bc9befff8e
Ivan <mozgol33@gmail.com>
2023-04-06 21:10:29 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void printarray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position< count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+int[] array = new int[10];
+
+FillArray(array);
+printarray(array);
